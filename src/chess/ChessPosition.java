c585a27696d316c8b78725de36206a54b6ef1b98
Lucas Bueno <lucasbuenno@outlook.com>
2024-06-25 20:44:38 -0300

    Moving piece

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,15 +1,15 @@
 package chess;
 
-import boardgame.BoardException;
 import boardgame.Position;
 
 public class ChessPosition {
+
     private char column;
     private int row;
 
     public ChessPosition(char column, int row) {
-        if(column < 'a' || column > 'h' || row < 1 || row > 8 ) {
-            throw new ChessException("Valid values are from a1 to h8");
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+            throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
         }
         this.column = column;
         this.row = row;
@@ -32,7 +32,6 @@ public class ChessPosition {
     }
 
     @Override
-
     public String toString() {
         return "" + column + row;
     }
